6cd72b7a4084e72db546d44aaf547e50ae0d6b1a
Florian Patzl <if14b048@technikum-wien.at>
2015-04-22 16:32:33 +0200

    init main file

---
+++ treecheck.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+//#include "tree.h"
+
+int main()
+{
+	int in;
+	while (true) {
+		std::cin >> in;
+		if(std::cin.fail()) {
+			break;
+		}
+
+		//insert
+
+		std::cout << in << std::endl;
+	}
+
+	//print
+
+	return 0;
+}
